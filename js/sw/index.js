4cc54e0b0507c9b92cc72e152618c3cfc7611fc2
Mohammed Riaad <MOhammed.riaad@gmail.com>
2018-05-08 23:45:52 +0200

    Add files via upload

---
+++ js/sw/index.js
@@ -0,0 +1,55 @@
+/*jshint esversion: 6 */
+
+var staticCacheName = 'restaurant-cahce-1';
+let urlToCache = [
+    '/skeleton',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'js/dbhelper.js',
+    'css/styles.css',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+    '//normalize-css.googlecode.com/svn/trunk/normalize.css"',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+];
+self.addEventListener('install', function (event) {
+    console.log('work fine');
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll(urlToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
---
+++ js/sw/sw.js
@@ -0,0 +1,57 @@
+var staticCacheName = 'restaurant-cahce-1';
+
+let urlToCache = [
+    '/',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/js/dbhelper.js',
+
+];
+self.addEventListener('install', function (event) {
+
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            console.log(cache);
+            return cache.addAll(urlToCache);
+
+        }).catch(erroe => {
+            console.log(erroe);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
